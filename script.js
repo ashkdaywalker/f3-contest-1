9479f8e5a29d5b488a522746d2b78ccd2653f704
ashkdaywalker <116715716+ashkdaywalker@users.noreply.github.com>
2023-02-11 12:46:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("fetching Data");
+
+"fetch('https://dummyjson.com/products')
+.then(res => res.json())
+.then(console.log);"
+let myDiv = document.getElementId("results");
+myDiv.innerHtml = <p><strong>${Data.id} - ${data.author}</strong>${data.quote}</p>
+console.log(myData);
